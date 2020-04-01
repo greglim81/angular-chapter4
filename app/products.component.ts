bafefbfe19ea9824a7b65484c55a2e222487fe6d
Greg Lim <limjunqu@gmail.com>
2020-04-01 11:44:31 +0800

    Update products.component.ts

--- app/products.component.ts
+++ app/products.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core'
-import { ProductService} from './product.service'
+import { ProductService } from './product.service'
 
 @Component({
     selector: 'products',
     template: `
-            <h2>Products</h2>        
-            <div *ngFor="let product of products">
-                <product [data]="product"></product>                
-            </div>
-        
-        `,
+        <h2>Products</h2>
+        <div *ngFor="let product of products">
+            <product [data]="product"></product>                
+        </div>  
+    `,
     providers: [ProductService]    
 })
 export class ProductsComponent{
@@ -18,4 +17,5 @@ export class ProductsComponent{
     constructor(productService: ProductService){
         this.products = productService.getProducts();
     }
+
 } 
